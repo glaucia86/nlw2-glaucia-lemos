d582a025acf03296dc5a096a1dce6af4441dafe1
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-04 01:49:33 -0300

    Desenvolvimento do arquivo 'global.css'

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './assets/styles/global.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Aumente o som... tão me ouvindo?!</h1>
+
+      <button type="button">
+        Clica em mim.
+      </button>
+      
     </div>
   );
 }
