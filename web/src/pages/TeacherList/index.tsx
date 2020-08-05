d89d95ca67e632a011c2706a9485427777a7fb7a
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-05 02:26:05 -0300

    Desenvolvimento da página 'TeacherList'

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg'
+
+import './styles.css';
 
 function TeacherList() {
-  return(
-    <h1>TeacherList</h1>
-  )
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar" />
+          </Link>
+          <img src={logoImg} alt="Logo Proffy" />    
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os Proffys disponíveis</strong>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeacherList;
