af28d75e03e6f046e4f6507e59d5bc37b3ea3ffa
glaucia86 <glaucia_lemos86@hotmail.com>
2020-08-05 03:52:05 -0300

    Desenvolvimento do styles.css do PageHeader

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 interface PageHeaderProps {
   title:string
 }
@@ -20,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
         <div className="header-content">
           <strong>{props.title}</strong>
+          {props.children}
         </div>
       </header>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import PageHeader from '../../components/PageHeader';
+import PageHeader from "../../components/PageHeader";
 
-import './styles.css';
+import "./styles.css";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Estes são os Proffys disponíveis." />
+      <PageHeader title="Estes são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da Semana</label>
+            <input type="text" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   );
 }
